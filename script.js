69bd09c3d81f5b86d0e5371eb18bc5003af59a10
DJacobson17 <83249575+DJacobson17@users.noreply.github.com>
2023-06-22 12:11:00 -0500

    fixed script to calculate and show

---
+++ script.js
@@ -0,0 +1,31 @@
+  // Tip Calculations
+  // document.getElementById("compute").onclick = handleClick();
+
+  function handleClick() {
+    
+    const tipscc = parseFloat(document.getElementById("tipsCc").value);
+    const tipscash = parseFloat(document.getElementById("tipsCash").value);
+    const busserPercent = (parseInt(document.getElementById("busserPer").value)) / 100;
+    let bartips;
+    let runner;
+    let tipsTotal = (tipscc + tipscash);
+    
+    bartips =  Math.ceil(tipsTotal * 0.05);
+    runner = Math.ceil(tipsTotal * 0.05);
+    console.log(bartips);
+      
+    busserTips = +(tipsTotal * busserPercent).toFixed(2);
+    totaltipout = bartips + busserTips + runner;
+    netTips = tipsTotal - totaltipout;
+    netTips = +netTips.toFixed(2);
+    console.log(busserTips);
+
+    document.getElementById("totaltips").append(tipsTotal);
+    document.getElementById("barTipout").append(bartips);
+    document.getElementById("busserTipout").append(busserTips);
+    document.getElementById("runnerTipout").append(runner);
+    document.getElementById("netTips").append(netTips);
+    document.getElementById("results").style.display="block";
+
+      
+    }
